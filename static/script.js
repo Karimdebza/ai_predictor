c81a244ac567d601f212582926f8979728d35f37
Karimdebza <debzakarim10@gmail.com>
2026-04-02 12:01:36 +0200

    graph

--- static/script.js
+++ static/script.js
@@ -1,27 +1,31 @@
 async function predict() {
-    const device = document.getElementById("device").value;
-    const days = document.getElementById("days").value;
+    const devise = document.getElementById("devise").value; 
+    const days = parseInt(document.getElementById("days").value);
 
-    const res = await fetch(`/predict?devise=${device}&days=${days}`)
+    const res = await fetch(`/predict?devise=${devise}&days=${days}`);
     const data = await res.json();
 
+   
     const trace1 = {
         x: data.dates,
-        y: data.hisoric,
+        y: data.historic,        
         mode: 'lines+markers',
         name: 'Historique',
-        line:{color:'blue'}
-    }
-    const trace2 ={
+        line: { color: 'blue' }
+    };
+
+    const trace2 = {
         x: data.pred_dates,
         y: data.predictions,
         mode: 'lines+markers',
-        name: 'Predictions',
-        line:{color:'red',dash:'dash'}
-    }
-    plotly.newPlot('plot', [trace1, trace2], {
-        title: `Predictions EUR > ${device}`,
+        name: 'Prédictions',
+        line: { color: 'red', dash: 'dash' }
+    };
+
+
+    Plotly.newPlot('chart', [trace1, trace2], {
+        title: `Prédictions EUR → ${devise}`,
         xaxis: { title: 'Date' },
-        yaxis: { title: 'Price' }
+        yaxis: { title: 'Taux' }
     });
 }
